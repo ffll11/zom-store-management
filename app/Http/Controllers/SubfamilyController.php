f8cdee2430762c233d8f0e9b510e5b9b5dbec662
FL <n00143543@upn.pe>
2025-10-14 23:19:33 -0500

    Initial commit

---
+++ app/Http/Controllers/SubfamilyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subfamily;
+use App\Http\Requests\StoreSubfamilyRequest;
+use App\Http\Requests\UpdateSubfamilyRequest;
+
+class SubfamilyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSubfamilyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Subfamily $subfamily)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Subfamily $subfamily)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSubfamilyRequest $request, Subfamily $subfamily)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Subfamily $subfamily)
+    {
+        //
+    }
+}
---
+++ database/seeders/SubfamilySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubfamilySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
